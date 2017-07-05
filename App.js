600c538c1a394c3a8ccda4eb7d255049f4eaf4cf
Emily Intersimone <emily.intersimone@gmail.com>
2017-07-05 17:45:33 -0400

    add event toggling

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
-import { Alert } from 'react-native';
+import { View, Button } from 'react-native';
 import Calendar from 'react-native-calendar'
+
+const customStyle = {
+  hasEventCircle: {
+    backgroundColor: 'yellow',
+  },
+}
 
 export default class App extends Component {
   constructor(props){
@@ -8,19 +14,46 @@ export default class App extends Component {
     this.state = {
       dates: []
     }
+
+    this.formatDate = this.formatDate.bind(this);
+    this.hasEvent = this.hasEvent.bind(this);
+    this.toggleEvent = this.toggleEvent.bind(this);
+  }
+
+  toggleEvent(date) {
+    date = this.formatDate(date);
+    if (!this.hasEvent(date)) this.setState({ 
+      dates: [...this.state.dates, date] 
+    })
+    else {
+      const idx = this.state.dates.indexOf(date)
+      this.setState({
+        dates: this.state.dates.slice(0, idx).concat(this.state.dates.slice(idx + 1))
+      });
+    }
+  }
+
+  formatDate(date) {
+    return date.split("T")[0];
+  }
+
+  hasEvent(date) {
+    return this.state.dates.indexOf(date) === -1 ? false : true;
   }
 
   render(){
     return (
-      <Calendar 
+      <View>
+        <Calendar 
         weekStart={0}
-        onDateSelect={(date) => {
-            this.setState({
-              dates: [...this.state.dates, date]
-            }, () => console.log(this.state.dates))
-          }
-        }
+        showEventIndicators={true}
+        eventDates={this.state.dates}
+        onDateSelect={this.toggleEvent}
+        customStyle={customStyle}
       />
+      <Button title="Schedule" onPress={() => {}} />
+      </ View>
     )
   }
 }
+
